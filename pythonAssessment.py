03f9006626f54792be1b3572b100fc61ecc18861
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-20 22:05:59 -0400

    Initial commit to github

---
+++ pythonAssessment.py
@@ -0,0 +1,10 @@
+import re
+from collections import Counter
+
+
+def load_text(filepath):
+    """Read the entire file and return its contents as one string."""
+
+
+def tokenize_words(text):
+    """Return a list of all word tokens (Lettters, digits, underscore)."""
