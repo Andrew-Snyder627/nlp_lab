ab690513c2aa6556017ee952952ebef387fda865
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-20 22:10:28 -0400

    Sets up initial scaffold for each function and main scripting

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -8,3 +8,30 @@ def load_text(filepath):
 
 def tokenize_words(text):
     """Return a list of all word tokens (Lettters, digits, underscore)."""
+
+
+def count_word(words, target):
+    """Count exact matches of target in the word list."""
+
+
+def most_common_word(words):
+    """Return the (word, count) pair for the most frequent word."""
+
+
+def average_word_length(words):
+    """Compute average length of words."""
+
+
+def count_paragraphs(text):
+    """Paragraphs defined as blocks seperated by one or more blank lines."""
+
+
+def count_sentences(text):
+    """
+    Sentences defined by splitting on . ! or ?.
+    We will count only non-empty segments.
+    """
+
+
+def main():
+    """Main script to prep article, determine target word, & display results"""
